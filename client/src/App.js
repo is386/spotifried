a9ae982bbb7494afb7d052389051e105de6ab454
is386 <20195347+is386@users.noreply.github.com>
2020-08-06 16:11:57 -0400

    add basic create account page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Create from "./Create";
+import Login from "./Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return null;
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/create" component={Create} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
