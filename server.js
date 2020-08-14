ebc45b08f3a60c9cf50c4ad888f6e21b1189d5a6
is386 <20195347+is386@users.noreply.github.com>
2020-08-14 17:41:21 -0400

    fix top 10 response bug

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
 
 const port = 5000;
 const hostname = "localhost";
 
+app.use(express.static("public_html")).use(cors()).use(cookieParser());
+
 const userRouter = require("./routes/user.js");
 app.use(userRouter);
+
+const top10Router = require("./routes/spotify.js");
+app.use(top10Router);
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
