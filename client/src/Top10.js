69c4ed20bc91cea132508349470a52be0fb53322
is386 <is386@drexel.edu>
2020-08-15 00:09:56 -0400

    render top 10 table

--- client/src/Top10.js
+++ client/src/Top10.js
@@ -3,6 +3,7 @@ import Spotify from "spotify-web-api-js";
 import "./App.css";
 
 const spotify = new Spotify();
+const songLimit = 10;
 
 class Top10 extends React.Component {
   constructor(props) {
@@ -20,6 +21,8 @@ class Top10 extends React.Component {
 
     this.getHashParams = this.getHashParams.bind(this);
     this.getTopTen = this.getTopTen.bind(this);
+    this.parseSongs = this.parseSongs.bind(this);
+    this.generateTable = this.generateTable.bind(this);
   }
 
   getHashParams() {
@@ -40,15 +43,53 @@ class Top10 extends React.Component {
       this.setState({ error: "Spotify not connected." });
       return;
     }
+    spotify.getMyTopTracks({ limit: songLimit }).then((response) => {
+      this.parseSongs(response.items);
+    });
+  }
+
+  parseSongs(songData) {
+    if (songData.length === 0) {
+      return "No Songs";
+    }
     let songs = [];
-    spotify.getMyTopTracks({ limit: 50 }).then((response) => {
-      if (response.items.length === 0) {
-        songs.push("No Songs");
+    for (let i = 0; i < songData.length; i++) {
+      let data = songData[i];
+      let artists = [];
+      for (let j = 0; j < data.artists.length; j++) {
+        artists.push(data.artists[j].name);
       }
-      for (let i = 0; i < response.items.length; i++) {
-        songs.push(response.items[i].name + "\n,");
-      }
-      this.setState({ songs: songs });
+      let song = {
+        id: i,
+        name: data.name,
+        album: data.album.name,
+        artists: artists,
+        art: data.album.images[0].url,
+        song_url: data.external_urls.spotify,
+        album_url: data.album.external_urls.spotify,
+      };
+      songs.push(song);
+    }
+    this.setState({ songs: songs });
+  }
+
+  generateTable() {
+    return this.state.songs.map((song) => {
+      const { id, name, album, artists, art, song_url, album_url } = song;
+      return (
+        <tr key={id}>
+          <td>
+            <img src={art} alt="album art" width="100" height="100" />
+          </td>
+          <td>
+            <a href={song_url}>{name}</a>
+          </td>
+          <td>
+            <a href={album_url}>{album}</a>
+          </td>
+          <td>{artists.join(", ")}</td>
+        </tr>
+      );
     });
   }
 
@@ -59,15 +100,26 @@ class Top10 extends React.Component {
     }
     return (
       <div onLoad={this.handleGetTop10}>
-        <h1>Top 10</h1>
+        <h1>Your Top 10</h1>
         <div id="error">{this.state.error}</div>
         <br />
         {spotifyConnect}
         <br />
         <button id="send" onClick={this.getTopTen}>
-          Get Top tracks
+          Get Top 10 Tracks
         </button>
-        <div id="songs">{this.state.songs}</div>
+        <br />
+        <table id="top-ten-songs">
+          <thead>
+            <tr>
+              <th>Cover Art</th>
+              <th>Track</th>
+              <th>Album</th>
+              <th>Artist(s)</th>
+            </tr>
+          </thead>
+          <tbody>{this.generateTable()}</tbody>
+        </table>
       </div>
     );
   }
